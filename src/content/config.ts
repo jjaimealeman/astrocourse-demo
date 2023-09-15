5b86d6815bb9089d7df3884c8533e2418f3b5378
Jaime Aleman <jjaimealeman@gmail.com>
2023-09-14 22:17:59 -0600

    Define z.objects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,25 @@
-import { defineCollection } from "astro:content";
+// 1. IMPORT UTILITIES FROM ASTRO:CONTENT
+import { z, defineCollection } from "astro:content";
+import { format } from "date-fns";
 
-const postCollection = defineCollection({
-  // The path to this collection.
+// 2. DEFINE YOUR COLLECTIONS
+const postsCollection = defineCollection({
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z
+      .string()
+      .transform((str: string | number | Date) =>
+        format(new Date(str), "MMMM d, yyyy")
+      ),
+    featured: z.boolean().default(false),
+    image: z.string(),
+    title: z.string(),
+  }),
 });
 
+// 3. EXPORT A SINGLE 'COLLECTIONS' OBJECT TO REGISTER YOUR COLLECTION(S)
+//    THIS KEY SHOULD MATCH YOUR COLLECTION DIRECTORY NAME IN /src/content
 export const collections = {
-  posts: postCollection,
+  posts: postsCollection,
 };
