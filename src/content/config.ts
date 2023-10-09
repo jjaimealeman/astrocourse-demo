4956ff54cff7af6b9a83f8b3dfaf472d3d316816
Jaime Aleman <jjaimealeman@gmail.com>
2023-10-08 18:14:27 -0600

    Fix config to reflect image object

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,19 +4,20 @@ import { format } from "date-fns";
 
 // 2. DEFINE YOUR COLLECTIONS
 const postsCollection = defineCollection({
-  schema: z.object({
-    author: z.string(),
-    categories: z.array(z.string()),
-    date: z
-      .string()
-      .transform((str: string | number | Date) =>
-        format(new Date(str), "MMMM d, yyyy")
-      ),
-    featured: z.boolean().default(false),
-    image: z.string(),
-    title: z.string(),
-    description: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      categories: z.array(z.string()),
+      date: z
+        .string()
+        .transform((str: string | number | Date) =>
+          format(new Date(str), "MMMM d, yyyy")
+        ),
+      featured: z.boolean().default(false),
+      image: image(),
+      title: z.string(),
+      description: z.string(),
+    }),
 });
 
 const testCollection = defineCollection({
