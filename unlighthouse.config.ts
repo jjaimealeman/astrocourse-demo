6067a7f47d54d8faf44e468adc77a9e3deb084cd
Jaime Aleman <jjaimealeman@gmail.com>
2023-10-08 19:35:23 -0600

    Fix unlighthouse.config

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,18 +1,15 @@
-/// <reference types="unlighthouse" />
-import { defineConfig } from "unlighthouse";
-
-export default defineConfig({
+export default {
   // examplebtn-basic
   site: "astrocourse-demo.netlify.app",
+  scanner: {
+    sitemap: ["/sitemap-0.xml", "/sitemap-index.xml"],
+    samples: 3,
+    // use desktop to scan
+    device: "desktop",
+  },
   //   scanner: {
   //     exclude: ['/private-zone/*']
   //   },
   // run lighthouse for each URL 3 times
-  samples: 3,
-  // use desktop to scan
-  device: "desktop",
   debug: true,
-  scanner: {
-    sitemap: ["/sitemap-0.xml", "/sitemap-index.xml"],
-  },
-});
+};
